1ba2ddaeea1c234e734bf479d56bd5a5119fd079
Kwasque <verdeymeremy@gmail.com>
2019-11-26 16:43:16 +0100

    Add: exercise-1

---
+++ exercise-1/string.js
@@ -0,0 +1,19 @@
+function ucfirst(str) {
+    if(typeof str !== "string" || !str) return '';
+    
+    return str[0].toUpperCase() + str.substring(1);
+}
+
+function capitalize(str) {
+    if(typeof str !== "string" || !str) return '';
+
+    return str.toLowerCase().split(" ").map(function(item) {
+        return ucfirst(item);
+    }).join(" ");
+}
+
+function camelCase(str) {
+    if(typeof str !== "string" || !str) return '';
+
+    return capitalize(str).replace(/\s/gi, "");
+}
