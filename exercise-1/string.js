373ca0b72a25067042f2e2b8e1fdcbbf4952cc65
Kwasque <verdeymeremy@gmail.com>
2019-11-26 17:12:48 +0100

    Add: leet

--- exercise-1/string.js
+++ exercise-1/string.js
@@ -30,8 +30,6 @@ function leet(str) {
     return str.replace(/a/gi, "4").replace(/e/gi, "3").replace(/i/gi, "1").replace(/o/gi, "0").replace(/u/gi, "(_)").replace(/y/gi, "7");
 } 
 
-console.log(leet('anaconda'));
-
 function prop_access(str) {
     if(typeof str !== "string" || !str) return '';
 
