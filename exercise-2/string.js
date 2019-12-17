02d182c967006a7bc3e6740f57abb0821617f45e
Rémy <verdeymeremy@gmail.com>
2019-12-17 10:54:42 +0100

    Fix: exercise-2

--- exercise-2/string.js
+++ exercise-2/string.js
@@ -1,27 +1,9 @@
 function type_check_v1(variable, type) {
     switch (type.toLowerCase()) {
-        case "string":
-            return typeof(variable) == type.toLowerCase();
-            break;
-        case "number":
-            return typeof(variable) == type.toLowerCase();
-            break;
-        case "boolean":
-            return typeof(variable) == type.toLowerCase();
-            break;
-        case "undefined":
-            return typeof(variable) == type.toLowerCase();
-            break;
-        case "function":
-            return typeof(variable) == type.toLowerCase();
-            break;
         case "null":
             return ((typeof(variable) == "object") && (variable == null));
             break;
-        case "object":
-            return typeof(variable) == type.toLowerCase();
-            break;
         default:
-            return undefined;
+            return typeof(variable) == type.toLowerCase();
     }
 }
