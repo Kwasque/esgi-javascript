66c3c17a09e887ba9f6b2c866e33d4c933e557e9
Rémy <verdeymeremy@gmail.com>
2019-12-17 11:00:40 +0100

    Fix: exercise-2

--- exercise-2/type-check.js
+++ exercise-2/type-check.js
@@ -7,3 +7,15 @@ function type_check_v1(variable, type) {
             return typeof(variable) == type.toLowerCase();
     }
 }
+
+function type_check_v2(variable, conf) {
+    if (conf.hasOwnProperty('type')) {
+        return type_check_v1(variable, conf.type);
+    }
+
+    if (conf.hasOwnProperty('value')) {
+
+    }
+
+    return true
+}
